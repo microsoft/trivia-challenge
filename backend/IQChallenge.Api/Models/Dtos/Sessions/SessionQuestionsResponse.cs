faba7f02f9fe6b3f5ee1308884503fbe0520b231
Christopher MANEU <git@maneu.org>
2026-02-10 09:11:41 +0100

    chore: Delete old structure

--- backend/IQChallenge.Api/Models/Dtos/Sessions/SessionQuestionsResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace TriviaChallenge.Api.Models.Dtos.Sessions;
-
-/// <summary>
-/// Response containing all questions for a session
-/// </summary>
-public class SessionQuestionsResponse
-{
-    public required List<SessionQuestionDto> Questions { get; set; }
-}
